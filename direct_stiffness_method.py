29e6a5372ca9e2f779267d1df40ac8c7bdb8ca59
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-02-26 03:53:13 -0500

    Update direct_stiffness_method.py

--- direct_stiffness_method.py
+++ direct_stiffness_method.py
@@ -7,13 +7,13 @@ class StiffnessMat:
     def __init__(self, frame_obj):
         self.frame = frame_obj
         self.n_points = self.frame.points.shape[0]
-        self.n_connectivities = self.frame.connectivity.shape[0]
+        self.n_connectivities = self.frame.connectivities.shape[0]
         self.n_DoFs = self.n_points * 6
 
     def get_stiffmat_global( self ):
         K = np.zeros(( self.n_DoFs, self.n_DoFs ) )
         for i in range( self.n_connectivities ):
-            connection = self.frame.connectivity[i]
+            connection = self.frame.connectivities[i]
             p0_idx = connection[0]
             p1_idx = connection[1]
 
@@ -27,9 +27,11 @@ class StiffnessMat:
             Iy = self.frame.Iy_array[ i ]
             Iz = self.frame.Iz_array[ i ]
             J = self.frame.J_array[ i ]
+            # change None stuff for this
+            I_z = self.frame.v_temp_array[i ]
 
             k_e_local = local_elastic_stiffness_matrix_3D_beam(E, nu, A, L, Iy, Iz, J )
-            gamma = rotation_matrix_3D(p0[0], p0[1], p0[2], p1[0], p1[1], p1[2])
+            gamma = rotation_matrix_3D(p0[0], p0[1], p0[2], p1[0], p1[1], p1[2], I_z)
             Gamma = transformation_matrix_3D(gamma)
             k_e_global = Gamma.T @ k_e_local @ Gamma
             k_e_p00 = k_e_global[0:6, 0:6]
