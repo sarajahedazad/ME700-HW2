d4d9998069b6eb88c2026239a32c977ea69e29cc
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-03-06 00:00:11 -0500

    Update test_geometry.py

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -52,7 +52,7 @@ def test_generate_frame_directly():
     Iz_array = np.array([1e-6])
     I_rho_array = np.array([1e-6])
     J_array = np.array([1e-6])
-    v_temp_array = None
+    v_temp_array = np.array([None])
 
     frame = Frame()
     frame.generate_frame_directly(points, connectivities, E_array, nu_array, A_array, Iy_array, Iz_array, I_rho_array, J_array, v_temp_array)
@@ -66,8 +66,28 @@ def test_generate_frame_directly():
     assert (frame.Iz_array == Iz_array).all()
     assert (frame.I_rho_array == I_rho_array).all()
     assert (frame.J_array == J_array).all()
-    if v_temp_array is not None:
-        assert (frame.v_temp_array == v_temp_array).all()
-    else:
-        assert (frame.v_temp_array == np.zeros(len(E_array))).all()
+    assert (frame.v_temp_array == v_temp_array).all()
     assert np.isclose(frame.L_array, np.sqrt(3)).all()
+
+def test_duplicate_elements():
+    frame = Frame()
+    p0 = frame.add_point(0, 0, 0)
+    p1 = frame.add_point(1, 1, 1)
+    element1 = frame.add_element(p0, p1, 210e9, 0.3, 0.01, 1e-6, 1e-6, 1e-6, 1e-6)
+    element2 = frame.add_element(p0, p1, 210e9, 0.3, 0.01, 1e-6, 1e-6, 1e-6, 1e-6)
+    try:
+        frame.build_frame([element1, element2])
+    except DuplicationError:
+        print("DuplicationError successfully raised")
+
+def test_existing_point():
+    frame = Frame()
+    p0 = frame.add_point(0, 0, 0)
+    frame.points_unique[(0, 0, 0)] = 0
+    p1 = frame.add_point(1, 1, 1)
+    element1 = frame.add_element(p0, p1, 210e9, 0.3, 0.01, 1e-6, 1e-6, 1e-6, 1e-6)
+    element2 = frame.add_element(p1, p0, 210e9, 0.3, 0.01, 1e-6, 1e-6, 1e-6, 1e-6)
+    frame.build_frame([element1, element2])
+    assert (frame.points == np.array([[0, 0, 0], [1, 1, 1]])).all()
+    assert (frame.connectivities == np.array([[0, 1], [1, 0]])).all()
+
